583c0a2c4b801c807b7fcef3b99131f79abdb7be
SHINICHIRO SUZUKI <shin@sforzando.co.jp>
2022-08-01 23:25:27 +0900

    feat: :+1:  introduce storybook-addon-next

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,9 @@
+import '../styles/globals.scss'
+
 export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
+  actions: {
+    argTypesRegex: '^on[A-Z].*',
+  },
   controls: {
     matchers: {
       color: /(background|color)$/i,
---
+++ stories/StyledButton.stories.tsx
@@ -0,0 +1,55 @@
+import { ComponentMeta } from '@storybook/react'
+import { useState } from 'react'
+import { StyledButton, StyledButtonProps } from '../components/StyledButton'
+import { action } from '@storybook/addon-actions'
+
+export default {
+  title: 'StyledButton',
+  component: StyledButton,
+  argTypes: {
+    onClick: { action: 'clicked' },
+  },
+} as ComponentMeta<typeof StyledButton>
+
+const incrementAction = action('increment')
+export const Primary = (props: StyledButtonProps) => {
+  const [count, setCount] = useState(0)
+  const onClick = (e: React.MouseEvent<HTMLElement>) => {
+    incrementAction(e, count)
+    setCount((c) => c + 1)
+  }
+
+  return (
+    <StyledButton {...props} variant='primary' onClick={onClick}>
+      Primary: {count}
+    </StyledButton>
+  )
+}
+
+export const Success = (props: StyledButtonProps) => {
+  const [count, setCount] = useState(0)
+  const onClick = (e: React.MouseEvent<HTMLElement>) => {
+    incrementAction(e, count)
+    setCount((c) => c + 1)
+  }
+
+  return (
+    <StyledButton {...props} variant='success' onClick={onClick}>
+      Success: {count}
+    </StyledButton>
+  )
+}
+
+export const Transparent = (props: StyledButtonProps) => {
+  const [count, setCount] = useState(0)
+  const onClick = (e: React.MouseEvent<HTMLElement>) => {
+    incrementAction(e, count)
+    setCount((c) => c + 1)
+  }
+
+  return (
+    <StyledButton {...props} variant='transparent' onClick={onClick}>
+      Transparent: {count}
+    </StyledButton>
+  )
+}

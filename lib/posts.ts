b60bced81b397ef26c261b45a03d107229fa0868
SHINICHIRO SUZUKI <shin@sforzando.co.jp>
2022-08-02 00:20:54 +0900

    fix: :bug:  lib/posts.ts

--- lib/posts.ts
+++ lib/posts.ts
@@ -19,7 +19,9 @@ export function getSortedPostsData() {
     }
   })
   return allPostsData.sort((a, b) => {
-    if (a.date < b.date) {
+    if (a.date == b.date) {
+      return 0
+    } else if (a.date < b.date) {
       return 1
     } else {
       return -1

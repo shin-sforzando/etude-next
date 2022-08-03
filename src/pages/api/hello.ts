ba5fcf128197f021987079b0eb686ed9a840138c
SHINICHIRO SUZUKI <shin@sforzando.co.jp>
2022-08-03 16:44:22 +0900

    wip: :construction: testing next-axiom with middleware

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,10 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { withAxiom } from 'next-axiom'
 
 type HelloResponse = {
   text: string
 }
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<HelloResponse>) {
-  res.status(200).json({ text: 'Hello World.' })
+async function handler(req: NextApiRequest, res: NextApiResponse<HelloResponse>) {
+  res.status(200).json({ text: 'Good bye, World.' })
 }
+
+export default withAxiom(handler)

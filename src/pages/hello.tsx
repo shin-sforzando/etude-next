79a55f35dbc55f10d9ec31d5b609f2e69cd585f6
SHINICHIRO SUZUKI <shin@sforzando.co.jp>
2022-08-03 16:41:14 +0900

    wip: :construction: testing next-axiom

--- src/pages/hello.tsx
+++ src/pages/hello.tsx
@@ -2,14 +2,14 @@ import { useEffect, useState } from 'react'
 import { log } from 'next-axiom'
 
 export default function Hello() {
+  log.info('hello')
   const [data, setData] = useState({ text: '' })
   useEffect(() => {
     fetch('/api/hello')
       .then((res) => res.json())
       .then((data) => setData(data))
-      .catch((err) => console.error(err))
+      .catch((err) => log.error(err))
   }, [])
-  log.info(`Data: ${JSON.stringify(data)}`)
 
   return <div>{data.text}</div>
 }

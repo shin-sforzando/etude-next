34c7074cebb7f11db1d35d6cbae23603b3b1ae1f
SHINICHIRO SUZUKI <shin@sforzando.co.jp>
2022-08-05 00:43:50 +0900

    feat: :+1: add @storybook/addon-a11y

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,7 @@
 module.exports = {
   stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
+    '@storybook/addon-a11y',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
